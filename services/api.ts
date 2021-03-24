39b6c62433cf583615ee495a469265c82d814c45
Sigit Prabowo <perdosi.developer@gmail.com>
2021-03-24 21:10:05 +0700

    fix: api endpoint

--- services/api.ts
+++ services/api.ts
@@ -1,87 +1,90 @@
-import axios from "axios"
+import axios from 'axios'
 import FormData from 'form-data'
 
 const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+  baseURL: process.env.NEXTAUTH_URL + '/api'
 })
 
 const protectedAPI = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
-	withCredentials: true
+  baseURL: process.env.NEXTAUTH_URL + '/api',
+  withCredentials: true
 })
 
 export const registerAPI = async (payload) => {
-	try {
-		const response = await api.post(`/register`, payload)
+  try {
+    const response = await api.post(`/register`, payload)
 
-		return response.data
-	} catch (error) {
-		throw error
-	}
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const pricingAPI = async () => {
-	try {
-		const response = await api.get(`/pricing`)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    const response = await api.get(`/pricing`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const clearSessionAPI = async (payload) => {
-	try {
-		const response = await api.post(`/auth/clear-session`, payload)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    const response = await api.post(`/auth/clear-session`, payload)
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const invoiceStatusAPI = async () => {
-	try {
-		const response = await api.get(`/user/invoice`)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    const response = await api.get(`/user/invoice`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const uploadReceiptAPI = async (payload) => {
-	try {
-		const response = await api.post(`/user/upload-receipt`, payload)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    const response = await api.post(`/user/upload-receipt`, payload)
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const uploadImgBBAPI = async (payload) => {
-	try {
-		let formData = new FormData()
-		formData.append('image', payload)
-		formData.append('key', process.env.NEXT_PUBLIC_IMGBB_KEY)
-		const response = await axios.post(process.env.NEXT_PUBLIC_IMGBB_API_URL, formData)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    let formData = new FormData()
+    formData.append('image', payload)
+    formData.append('key', process.env.NEXT_PUBLIC_IMGBB_KEY)
+    const response = await axios.post(
+      process.env.NEXT_PUBLIC_IMGBB_API_URL,
+      formData
+    )
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const sendQuestionAPI = async (payload) => {
-	try {
-		const response = await api.post(`/user/question`, payload)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    const response = await api.post(`/user/question`, payload)
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
 
 export const courseAPI = async () => {
-	try {
-		const response = await api.post(`/course/list`)
-		return response.data
-	} catch (error) {
-		throw error
-	}
+  try {
+    const response = await api.post(`/course/list`)
+    return response.data
+  } catch (error) {
+    throw error
+  }
 }
